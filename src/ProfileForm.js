79129c5a7a4828305e042e48a237603d51d4338c
Mahathi1999 <124342355+Mahathi1999@users.noreply.github.com>
2023-06-03 17:33:13 +0530

    Update ProfileForm.js

--- src/ProfileForm.js
+++ src/ProfileForm.js
@@ -1,70 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
-import ProfileSchema from './ProfileSchema';
+import * as Yup from 'yup';
 
-const ProfileForm = () => (
-  <div>
-    <h1>Profile form</h1>
-    <Formik
-      initialValues={{ fullname: '', email: '', telephone: '' }}
-      validationSchema={ProfileSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 1000);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="fullnamme">Fullname</label>
-            <input
-              type="fullname"
-              name="fullname"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.fullname}
-            />
-            {errors.fullname && touched.fullname && errors.fullname}
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            {errors.email && touched.email && errors.email}
-          </div>
-          <div className="form-group">
-            <label htmlFor="telephone">Telephone</label>
-            <input
-              type="telephone"
-              name="telephone"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.telephone}
-            />
-            {errors.telephone && touched.telephone && errors.telephone}
-          </div>
-          <button type="submit" disabled={isSubmitting}>
-            {isSubmitting ? 'Submitting' : 'Submit'}
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>
-);
+const ProfileSchema = Yup.object().shape({
+  fullname: Yup.string().required('Fullname is required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  telephone: Yup.string().required('Telephone is required')
+});
+
+const ProfileForm = () => {
+  const [profiles, setProfiles] = useState([]);
+
+  const handleCreate = (values, { setSubmitting, resetForm }) => {
+    setTimeout(() => {
+      const newProfile = {
+        id: Math.random().toString(36).substr(2, 9), // Generate a unique ID
+        fullname: values.fullname,
+        email: values.email,
+        telephone: values.telephone
+      };
+      setProfiles([...profiles, newProfile]);
+      resetForm();
+      setSubmitting(false);
+      alert('Profile created successfully!');
+    }, 1000);
+  };
+
+  const handleDelete = (id) => {
+    const updatedProfiles = profiles.filter((profile) => profile.id !== id);
+    setProfiles(updatedProfiles);
+    alert('Profile deleted successfully!');
+  };
+
+  return (
+    <div>
+      <h1>Profile form</h1>
+      <Formik
+        initialValues={{ fullname: '', email: '', telephone: '' }}
+        validationSchema={ProfileSchema}
+        onSubmit={handleCreate}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="fullname">Fullname</label>
+              <input
+                type="text"
+                name="fullname"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.fullname}
+              />
+              {errors.fullname && touched.fullname && <div className="error">{errors.fullname}</div>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              {errors.email && touched.email && <div className="error">{errors.email}</div>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="telephone">Telephone</label>
+              <input
+                type="text"
+                name="telephone"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.telephone}
+              />
+              {errors.telephone && touched.telephone && <div className="error">{errors.telephone}</div>}
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? 'Submitting' : 'Create Profile'}
+            </button>
+          </form>
+        )}
+      </Formik>
+
+      <h2>Profiles</h2>
+      <ul>
+        {profiles.map((profile) => (
+          <li key={profile.id}>
+            <div>{`Fullname: ${profile.fullname}`}</div>
+            <div>{`Email: ${profile.email}`}</div>
+            <div>{`Telephone: ${profile.telephone}`}</div>
+            <button onClick={() => handleDelete(profile.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ProfileForm;
